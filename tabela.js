ae5bf825b3f90ec55ca02e5d246877106bf4dccb
Guilherme Moersbaecher <guimorshi@gmail.com>
2021-11-11 23:07:43 -0300

    primeira versão do tabuleiro de xadrez

--- tabela.js
+++ tabela.js
@@ -8,7 +8,7 @@ function alterar(){
     let color = ["lightgrey", "pink", "green", "red", "blue", 
     "orange", "purple", "lightblue", "lightgreen"];
 
-    color = color.sort(() => Math.random() - 0.1);
+    color = color.sort(() => Math.random() - 0.5);
 
         for(i=0;i<quadro.length;i++){            
             quadro[i].style.backgroundColor = color[i%9];
