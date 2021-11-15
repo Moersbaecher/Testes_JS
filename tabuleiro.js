f7821e817ae0c182799a71b8f94cd632ec2bf0d3
Guilherme Moersbaecher <guimorshi@gmail.com>
2021-11-15 00:15:57 -0300

    adicionado o setTimeOut(), placar certo/errado

--- tabuleiro.js
+++ tabuleiro.js
@@ -1,4 +1,7 @@
 let referencia = document.getElementById("referencia");
+let mostrar = document.getElementById("mostrar");
+let certo = document.getElementById("certo");
+let errado = document.getElementById("errado");
 
 let casa = ["A8",	"B8",	"C8",	"D8",	"E8",	"F8",	"G8",	"H8",
             "A7",	"B7",	"C7",	"D7",	"E7",	"F7",	"G7",	"H7",
@@ -9,12 +12,12 @@ let casa = ["A8",	"B8",	"C8",	"D8",	"E8",	"F8",	"G8",	"H8",
             "A2",	"B2",	"C2",	"D2",	"E2",	"F2",	"G2",	"H2",
             "A1",	"B1",	"C1",	"D1",	"E1",	"F1",	"G1",	"H1"];
 
-function mostrar(){
+mostrar.addEventListener("click", function() {
 casa = casa.sort(() => Math.random() - 0.5);
 
 for(i=0;i<casa.length;i++){            
     referencia.innerHTML = casa[i];
-}}
+}})
 
 
 let tabuleiro = document.createElement("table"),
@@ -25,16 +28,45 @@ casa.forEach((value, i) => {
     let placar = document.getElementById("placar");
 
   cell = row.insertCell();
-  cell.innerHTML = "";
- 
-   cell.onclick = function clicar(){
-       if(value == referencia.innerHTML) {
-        alert('Certo :]')
-       } else {alert('Errado :[')}
+  cell.innerHTML = value;
 
-       referencia.innerHTML = "";
-       //console.log(value);
+  
+  function certoPopup() {
+    var win = window.open( "img/certo.gif", "myWindow","height = 350, width = 350, resizable = 0" );
+    setTimeout( function() {
+        win.close();
+    }, 1000);}
+
+    function errdoPopup() {
+        var win = window.open( "img/nope.gif", "myWindow","height = 350, width = 350, resizable = 0" );
+        setTimeout( function() {
+            win.close();
+        }, 1000);
+}
+ 
+   cell.addEventListener("click", function (){
+       var add = 1;
+
+       if(value == referencia.innerHTML) {
+        certo.innerHTML = parseInt(certo.innerHTML) + add;
+        certoPopup()              
+       } else {errado.innerHTML = parseInt(errado.innerHTML) + add;
+        errdoPopup()
     }
+       
+       referencia.innerHTML = "";
+       //console.log(certo.innerHTML);
+       //console.log(add);
+
+       setTimeout(function() {
+        casa = casa.sort(() => Math.random() - 0.5);
+        
+        for(i=0;i<casa.length;i++){            
+            referencia.innerHTML = casa[i];
+        }}, 1000);
+        
+       
+    })
 
   let next = i + 1;
   if (next%porLinha==0 && next!=casa.length) { row = tabuleiro.insertRow(); }
