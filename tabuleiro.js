c02af36a16ccd6dff283eb7d4d00c4e344ed9511
Guilherme Moersbaecher <guimorshi@gmail.com>
2022-08-23 19:58:04 -0300

    inclusão qualCor

--- tabuleiro.js
+++ tabuleiro.js
@@ -3,8 +3,13 @@ let mostrar = document.getElementById("mostrar");
 let certo = document.getElementById("certo");
 let errado = document.getElementById("errado");
 let gif = document.getElementById("gif");
+let inverter = document.getElementById("inverter");
+let selecionaCor = document.getElementById("color");
+let mostraCor = document.getElementById("mostraCor");
 
 const files = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+//======================================================   FUNCTIONS   ======================================================\\
 
 function getRandomNumber(min, max) {
     return min + Math.floor(Math.random() * (max - min + 1));
@@ -18,17 +23,20 @@ mostrar.addEventListener("click", function () {
     referencia.innerHTML = getRandomSquare();
 })
 
-
 let tabuleiro = document.createElement("table"),
     row = tabuleiro.insertRow(), cell;
+       
 
-for(let i = 8; i > 0; i--) {
-    for(const file of files) {
-        const square = file+i;
+//======================================================   FOR   ======================================================\\
+
+for(let i = 8; i >= 1; i--) {
+    for(let j = 0; j <= 7; j++) {
+        const square = files[7 - j] + (9-i);
         cell = row.insertCell();
-        cell.innerHTML = "";
-    
-    
+        cell.innerHTML = '';
+
+        
+
         function certoPopup() {
             gif.style.backgroundImage = "url(img/certo.gif)";
             setTimeout(function () {
@@ -42,28 +50,45 @@ for(let i = 8; i > 0; i--) {
                 gif.style.backgroundImage = "";
             }, 1000);
         }
+
+    //=================================================   click   =================================================\\
     
         cell.addEventListener("click", function () {
-            var add = 1;
-    
+            let add = 1;
+
+           const square = selecionaCor.value == 'brancas'?files[j] + i:files[7 - j] + (9 - i);            
+
+           console.log("clique / " + files[j] + i);
+
             if (square == referencia.innerHTML) {
                 certo.innerHTML = parseInt(certo.innerHTML) + add;
+                console.log("if " + square);
                 certoPopup()
             } else {
                 errado.innerHTML = parseInt(errado.innerHTML) + add;
+                console.log("else " + square);
                 erradoPopup()
+            }
+
+
+            if(i % 2 == 1 && j % 2 == 0 || i % 2 == 0 && j % 2 == 1){
+                document.getElementById('mostraCor').style.backgroundColor = 'black';
+            } else{
+                document.getElementById('mostraCor').style.backgroundColor = 'white';
             }
     
             referencia.innerHTML = "";
     
             setTimeout(function () {
                 referencia.innerHTML = getRandomSquare();
-            }, 100);
-    
-    
+            }, 100);    
         })
+
     }
-    row = tabuleiro.insertRow();
+    row = tabuleiro.insertRow();    
 }
 
 document.getElementById("container").appendChild(tabuleiro);
+
+//===========================================================================================\\
+
